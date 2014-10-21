2c215520fd291373a9ae7be095837887a48e159a
AnAQuin Better <l_dn_near@hotmail.com>
2014-10-21 08:25:46 +0000

    add hello world

---
+++ application/views/helloworld.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<html>
+    <head>
+        
+    </head>
+    <body>
+        Hello World!!!
+        <a href="https://www.google.co.th"> ไปเลย </a>
+    </body>
+</html>
